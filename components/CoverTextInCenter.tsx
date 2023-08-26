e2bb87e3100ccf5820bb24b15e2110528259f5b7
Kirill Zhirnov <ekvixgroup@gmail.com>
2023-08-26 18:32:21 +0200

    transformed links to next13

--- components/CoverTextInCenter.tsx
+++ components/CoverTextInCenter.tsx
@@ -33,10 +33,10 @@ export default function CoverTextInCenter({img, imgPortrait, shadow, content, li
 				{shadow && <div className='cover-text-center__shadow' style={shadow} />}
 				<div className='cover-text-center__content'>
 					{link
-						? <Link href={link}>
-							<a className='cover-text-center__content-container container' {...linkProps}>
+						? <Link href={link}
+										className='cover-text-center__content-container container'
+										{...linkProps}>
 								<CoverContent {...content} />
-							</a>
 						</Link>
 						: <div className='cover-text-center__content-container container'>
 							<CoverContent {...content} />
